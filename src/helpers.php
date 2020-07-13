12b1a94f82c587f7bc9d39e46c971d8c06622d26
Aniket Magadum <aniketmagadum77@gmail.com>
2020-07-13 14:35:31 +0530

    Initial Commit

---
+++ src/helpers.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This function returns black or white based on the luminance of the color being passed.
+ *
+ * @param string $color
+ * @return string
+ */
+if (!function_exists('getContrastingColor')) {
+    function getContrastingColor($color)
+    {
+        $r = hexdec(substr($color, 1, 2));
+        $g = hexdec(substr($color, 3, 2));
+        $b = hexdec(substr($color, 5, 2));
+
+        if ($r + $g + $b > 382) {
+            return '#FFFFFF';
+        }
+        return '#000000';
+    }
+}
